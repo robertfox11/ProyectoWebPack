c73e9183328eb69cbd518d2cb2aa537a15042ef2
robert Salazar <11rsahome@gmail.com>
2019-12-10 17:07:13 +0100

    add jquery and funcion jquery

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     //la direccion de origen 
-    entry: path.join(__dirname, 'main'),
+    entry: path.join(__dirname, 'main', ),
     watch: true,
     output: {
         path: path.join(__dirname, 'dist'),
