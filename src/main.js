71d7b42fe248305cfac92911c587c43dce1d9bde
robert Salazar <11rsahome@gmail.com>
2020-01-05 17:39:14 +0100

    update img png webpack

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { card } from "./assets/js/module-a";
 import { color } from "./assets/js/module-b";
 
 import "./assets/scss/main.scss";
+import "./assets/img/56kb.svg";
+import "./assets/img/Edad_12kbpng.png";
 
 
 const btn = document.querySelector('#btn');
--- webpack.config.js
+++ webpack.config.js
@@ -5,34 +5,67 @@ const webpack = require("webpack");
 module.exports = {
     mode: 'development',
     //la direccion de origen 
-    entry: path.join(__dirname, './src/main.js'),
+    entry: './src/main.js',
     watch: true,
     output: {
-        path: path.join(__dirname, 'dist'),
-        // publicPath: '/dist/',
+        //creamos una carpeta de salida dist
+        path: path.resolve(__dirname, 'dist'),
+        // publicPath : '/dist/',
         filename: "bundle.js" //agrupa archivo
             // chunkFilename: '[name].js'
     },
     module: {
         rules: [{
                 test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
-                ],
+                use: [{
+                    loader: MiniCssExtractPlugin.loader
+                }, { loader: 'css-loader' }, { loader: 'sass-loader' }, ],
             },
-            { test: /\.txt$/, use: 'raw-loader' }
+            {
+                test: /\.txt$/,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true,
+                    },
+                }, ],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: '[hash:base64].[ext]',
+                        outputPath: 'assets/img',
+                        // useRelativePath: true,
+                        esModule: false
+
+                    },
+
+                }, ],
+            },
         ],
     },
     devServer: {
         port: 5000
     },
     plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
         new HtmlWebPackPlugin({
             //vamos a copiar este index para mandarlo al dist
-            template: './src/index.html'
+            template: './src/index.html',
+            filename: "./index.html"
         }),
         new MiniCssExtractPlugin({
-            // template: './src/main.scss',
+            // Creamos el codigo de salida Css
             filename: 'bundle.css'
         })
     ],
